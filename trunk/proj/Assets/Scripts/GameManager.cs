81648e459d23942b5ec27dab17531bcc0d6a5e8d
Tomasz.Bazan.89@gmail.com <Tomasz.Bazan.89@gmail.com@a605bf5d-e761-9721-1d0b-dbcb4f5d1c3e>
2013-05-15 19:15:40 +0000

    Szkielet klas GameManager oraz PlaceUnitState

---
+++ trunk/proj/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+namespace AssemblyCSharp
+{
+	public class GameManager : MonoBehaviour
+	{
+		//private HighScore highScoreInstance;
+		
+		private void Initialize()
+		{
+			throw new NotImplementedException();
+		}
+		
+		public GameManager Instance()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
+
---
+++ trunk/proj/Assets/Scripts/PlaceUnitState.cs
@@ -0,0 +1,73 @@
+using System;
+
+namespace AssemblyCSharp
+{
+	public class PlaceUnitState : GameState
+	{
+		private void SelectUnit(Unit unit)
+		{
+			throw new NotImplementedException();
+		}
+		
+		private void EndTurnButtonClickedHandler(Object sender, EventArgs args)
+		{
+			throw new NotImplementedException();			
+		}
+		
+		private void DeselectUnitButtonClickedHandler(Object sender, EventArgs args)
+		{
+			throw new NotImplementedException();			
+		}
+		
+		private void DeselectCurrnetUnit()
+		{
+			throw new NotImplementedException();			
+		}
+		
+		private void CreateUnitOnPosition(UnityEngine.Vector3 worldPosition, UnityEngine.Object prefab)
+		{
+			throw new NotImplementedException();			
+		}
+		
+		private void RemoveUnit(Unit unit)
+		{
+			throw new NotImplementedException();			
+		}
+		
+		private void BWPButtonClickedHandler(Object sender, EventArgs args)
+		{
+			throw new NotImplementedException();			
+		}
+		
+		private void TankButtonClickedHandler(Object sender, EventArgs args)
+		{
+			throw new NotImplementedException();			
+		}
+		
+		private void HeliButtonClickedHandler(Object sender, EventArgs args)
+		{
+			throw new NotImplementedException();			
+		}
+		
+		private void HeavyTankButtonClickedHandler(Object sender, EventArgs args)
+		{
+			throw new NotImplementedException();			
+		}
+		
+		private void AltyleryButtonClickedHandler(Object sender, EventArgs args)
+		{
+			throw new NotImplementedException();			
+		}
+		
+		private void UpdatePoints(Object sender, EventArgs args)
+		{
+			throw new NotImplementedException();			
+		}
+		
+		private void ContinueButtonClickedHandler(Object sender, EventArgs args)
+		{
+			throw new NotImplementedException();			
+		}
+	}
+}
+
