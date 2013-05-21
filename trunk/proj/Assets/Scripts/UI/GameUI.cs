1de7b419bfd6559a15b088dd463031d7c9e62e86
zbyszekprasak@gmail.com <zbyszekprasak@gmail.com@a605bf5d-e761-9721-1d0b-dbcb4f5d1c3e>
2013-05-21 15:56:53 +0000

    Implementacja klasy MainMenuUI. Fixes Issue 030.

--- trunk/proj/Assets/Scripts/UI/GameUI.cs
+++ trunk/proj/Assets/Scripts/UI/GameUI.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class GameUI : MonoBehaviour {
 	
+	public MainMenuUI MainMenuUIInstance;
 	public MapSelectionUI MapSelectionUIInstance;
 	public InGameUI InGameUIInstance;
+	
 }
---
+++ trunk/proj/Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,8 @@
+public class MainMenuUI : UILogic {
+
+	public ButtonLogic StartButton;
+	public ButtonLogic HelpButton;
+	public ButtonLogic ScoreButton;
+	public ButtonLogic ExitButton;
+	
+}
