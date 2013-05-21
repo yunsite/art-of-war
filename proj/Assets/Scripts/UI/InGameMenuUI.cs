097fd27151ec5f3241c0cb0c50f3659113da0d80
zbyszekprasak <zbyszekprasak@gmail.com>
2013-05-21 16:10:07 +0000

    Implementacja klasy InGameMenuUI. Fixes Issue 031.

---
+++ proj/Assets/Scripts/UI/InGameMenuUI.cs
@@ -0,0 +1,9 @@
+public class InGameMenuUI : UILogic {
+	
+	public ButtonLogic ResumeButton;
+	public ButtonLogic RestartButton;
+	public ButtonLogic MainMenuButton;
+	public ButtonLogic HelpButton;
+	public ButtonLogic QuitButton;
+	
+}
