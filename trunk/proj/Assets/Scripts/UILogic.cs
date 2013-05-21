702c90dcec426c9bbf4096487fa19329224174b2
zbyszekprasak@gmail.com <zbyszekprasak@gmail.com@a605bf5d-e761-9721-1d0b-dbcb4f5d1c3e>
2013-05-21 07:22:59 +0000

    Poprawa metod Show oraz Hide klasy UILogic, tak by następuowała aktywacja oraz dezktywacja obiektu sceny. Fixes Issue 025.

--- trunk/proj/Assets/Scripts/UILogic.cs
+++ trunk/proj/Assets/Scripts/UILogic.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public abstract class UILogic : MonoBehaviour {
 
 	public virtual void Show () {
-		enabled = true;
+		gameObject.SetActive(true);
 	}
 	
 	public virtual void Hide () {
-		enabled = false;
+		gameObject.SetActive(false);
 	}
 	
 }
