fe2ea4a77ad6662f1e68becc1f6ae8ae0a44cbf1
Tomasz.Bazan.89@gmail.com <Tomasz.Bazan.89@gmail.com@a605bf5d-e761-9721-1d0b-dbcb4f5d1c3e>
2013-05-23 16:58:44 +0000

    uzupełnienie klasy InGameState oraz PlaceUnitState

--- trunk/proj/Assets/Scripts/StateMachine/InGameState.cs
+++ trunk/proj/Assets/Scripts/StateMachine/InGameState.cs
@@ -1,11 +1,14 @@
 using System;
 using UnityEngine;
 
-
 public class InGameState : GameState
 {
 	
 	private InGameUI ui;
+	private Unit selectedUnit;
+	private bool isAtacking;
+	private bool isMoving;
+	private bool isBlocked;
 	
 	public InGameState (GameController controller) : base (controller) {
 		if (Application.loadedLevelName != "HillyTestScene") 
@@ -13,22 +16,36 @@ public class InGameState : GameState
 		GameManager manager = GameManager.Instance();
 		ui = manager.GameUIInstance.InGameUIInstance;
 		ui.EndTurnButton.ButtonClicked += EndTurnButtonClickedHandler;
+		ui.AttackButton.ButtonClicked += AttackButtonClickedHandler;
+		ui.MoveButton.ButtonClicked += MoveButtonClickedHandler;
 		ui.Show();
 	}
 	
 	private void ShowInGameMenu()
 	{
-		throw new NotImplementedException();
+		ui.Show();
 	}
 	
 	private void HideInGameMenu()
 	{
-		throw new NotImplementedException();
+		ui.Hide();
 	}
 	
 	private void UnitClickedEventHandler (object sender, EventArgs args)
 	{
-		throw new NotImplementedException();
+		if(!isAtacking && selectedUnit == null)
+		{
+			selectedUnit = (Unit) sender;
+		}
+		else if(!isAtacking && selectedUnit != (Unit) sender)
+		{
+			selectedUnit = (Unit) sender;
+		}
+		else if(isAtacking && selectedUnit != null)
+		{
+			selectedUnit.Attack((Unit) sender);
+			BlockUIInput();
+		}
 	}
 	
 	private void SelectUnit(Unit unit)
@@ -43,7 +60,16 @@ public class InGameState : GameState
 	
 	private void BlockUIInput()
 	{
-		throw new NotImplementedException();
+		ui.AttackButton.enabled = false;
+		ui.MoveButton.enabled = false;
+		ui.SpecialAbilityButton.enabled = false;
+	}
+	
+	private void UnBlockUIInput()
+	{
+		ui.AttackButton.enabled = true;
+		ui.MoveButton.enabled = true;
+		ui.SpecialAbilityButton.enabled = true;
 	}
 	
 	private void ShowSelectedUnitInfo()
@@ -81,19 +107,43 @@ public class InGameState : GameState
 		throw new NotImplementedException();
 	}
 	
-	public bool AttackEnded()
+	public void AttackEnded()
 	{
-		throw new NotImplementedException();
+		UnBlockUIInput();
 	}
 	
-	public bool MovementEnded()
+	public void MovementEnded()
 	{
-		throw new NotImplementedException();
+		UnBlockUIInput();
 	}
 	
 	public void EndTurnButtonClickedHandler(object sender, EventArgs args)
 	{
-		Debug.Log("End of tour button cicked");
+		isAtacking = false;
+		isMoving = false;
+		selectedUnit = null;
+		Debug.Log("End of tour button clicked");
+	}
+	
+	private void AttackButtonClickedHandler(object sender, EventArgs args)
+	{
+		isAtacking = true;
+		isMoving = false;
+	}
+	
+	private void MoveButtonClickedHandler(object sender, EventArgs args)
+	{
+		isMoving = true;
+		isAtacking = false;
+	}
+	
+	private void TerrainClickedHandler(object sender, EventArgs args) //PositionArgs args)
+	{
+		if(isMoving)
+		{
+			//selectedUnit.MoveToPosition(args.position);
+			BlockUIInput();
+		}
 	}
 }
 
--- trunk/proj/Assets/Scripts/StateMachine/PlaceUnitState.cs
+++ trunk/proj/Assets/Scripts/StateMachine/PlaceUnitState.cs
@@ -1,84 +1,89 @@
 using System;
 
-namespace AssemblyCSharp
+public class PlaceUnitState : GameState
 {
-	// <zbyszekprasak>
-	// Ta klasa sie nie kompiluje, proszę nie robić commitów składających się z takich klas.
-	// </zbyszekprasak>
-//	public class PlaceUnitState : GameState
-//	{
-//		private void SelectUnit(Unit unit)
-//		{
-//			unit.OnClick();
-//		}
-//		
-//		//co się dzieje po zakończeniu rundy?
-//		private void EndTurnButtonClickedHandler(Object sender, EventArgs args)
-//		{
-//			throw new NotImplementedException();			
-//		}
-//		
-//		private void DeselectUnitButtonClickedHandler(Object sender, EventArgs args)
-//		{
-//			DeselectCurrnetUnit();		
-//		}
-//		
-//		//nie wiem jak to ma działać
-//		private void DeselectCurrnetUnit()
-//		{
-//			throw new NotImplementedException();			
-//		}
-//		
-//		//nie wiem jak to ma działać
-//		private void CreateUnitOnPosition(UnityEngine.Vector3 worldPosition, UnityEngine.Object prefab)
-//		{
-//			throw new NotImplementedException();
-//		}
-//		
-//		private void RemoveUnit(Unit unit)
-//		{
-//			unit.Die();			
-//		}
-//		
-//		private void BWPButtonClickedHandler(Object sender, EventArgs args)
-//		{
-//			GameObject unit = (GameObject)Instantiate(Resources.Load("Prefab_BWPUnit"));
-//			CreateUnitOnPosition(new UnityEngine.Vector3(0,0,0), (Object) Unit);
-//		}
-//		
-//		private void TankButtonClickedHandler(Object sender, EventArgs args)
-//		{
-//			GameObject unit = (GameObject)Instantiate(Resources.Load("Prefab_TankUnit"));
-//			CreateUnitOnPosition(new UnityEngine.Vector3(0,0,0), (Object) Unit);
-//		}
-//		
-//		private void HeliButtonClickedHandler(Object sender, EventArgs args)
-//		{
-//			GameObject unit = (GameObject)Instantiate(Resources.Load("Prefab_HeliUnit"));
-//			CreateUnitOnPosition(new UnityEngine.Vector3(0,0,0), (Object) Unit);
-//		}
-//		
-//		private void HeavyTankButtonClickedHandler(Object sender, EventArgs args)
-//		{
-//			GameObject unit = (GameObject)Instantiate(Resources.Load("Prefab_HeavyTankUnit"));
-//			CreateUnitOnPosition(new UnityEngine.Vector3(0,0,0), (Object) Unit);
-//		}
-//		
-//		private void AltyleryButtonClickedHandler(Object sender, EventArgs args)
-//		{
-//			GameObject unit = (GameObject)Instantiate(Resources.Load("Prefab_AltyleryUnit"));
-//			CreateUnitOnPosition(new UnityEngine.Vector3(0,0,0), (Object) Unit);
-//		}
-//		
-//		//nie wiem jak to ma działać
-//		private void UpdatePoints(Object sender, EventArgs args)
-//		{
-//			throw new NotImplementedException();			
-//		}
-//		
-//		private void ContinueButtonClickedHandler(Object sender, EventArgs args)
-//		{
-//			GameState = GameController().GoToInGameState();			
-//		}
-//	}
+	//private SelectedUIInstance ui;
+	
+	public PlaceUnitState(GameController controller) : base (controller) 
+	{
+		if (UnityEngine.Application.loadedLevelName != "HillyTestScene") 
+			UnityEngine.Application.LoadLevel("HillyTestScene");
+		GameManager manager = GameManager.Instance();
+		//ui = manager.SelectedUIInstance.SelectedUIInstance;
+		//ui.BWPButton.ButtonClicked += BWPButtonClickedHandler;
+		//ui.TankButton.ButtonClicked += TankButtonClickedHandler;
+		//ui.HeliButton.ButtonClicked += HeliButtonClickedHandler;
+		//ui.HeavyTankButton.ButtonClicked += HeavyTankButtonClickedHandler;
+		//ui.AltyleryButton.ButtonClicked += AltyleryButtonClickedHandler;
+		//ui.Show();
+	}
+	
+	private void SelectUnit(Unit unit)
+	{
+		//unit.OnClick();
+	}
+
+	//co się dzieje po zakończeniu rundy?
+	private void EndTurnButtonClickedHandler(Object sender, EventArgs args)
+	{
+		throw new NotImplementedException();			
+	}
+	
+	private void DeselectUnitButtonClickedHandler(Object sender, EventArgs args)
+	{
+		DeselectCurrnetUnit();		
+	}
+	
+	//nie wiem jak to ma działać
+	private void DeselectCurrnetUnit()
+	{
+		throw new NotImplementedException();			
+	}
+	
+	//nie wiem jak to ma działać
+	private void CreateUnitOnPosition(UnityEngine.Vector3 worldPosition, UnityEngine.Object prefab)
+	{
+		throw new NotImplementedException();
+	}
+	
+	private void RemoveUnit(Unit unit)
+	{
+		unit.Die();			
+	}
+	
+	private void BWPButtonClickedHandler(Object sender, EventArgs args)
+	{
+		CreateUnitOnPosition(new UnityEngine.Vector3(0,0,0), UnityEngine.Resources.Load("Prefab_BWPUnit"));
+	}
+
+	private void TankButtonClickedHandler(Object sender, EventArgs args)
+	{
+		//CreateUnitOnPosition(new UnityEngine.Vector3(0,0,0), UnityEngine.Resources.Load("Prefab_TankUnit"));
+	}
+	
+	private void HeliButtonClickedHandler(Object sender, EventArgs args)
+	{
+		CreateUnitOnPosition(new UnityEngine.Vector3(0,0,0), UnityEngine.Resources.Load("PPrefab_HeliUnit"));
+	}
+	
+	private void HeavyTankButtonClickedHandler(Object sender, EventArgs args)
+	{
+		CreateUnitOnPosition(new UnityEngine.Vector3(0,0,0), UnityEngine.Resources.Load("Prefab_HeavyTankUnit"));
+	}
+	
+	private void AltyleryButtonClickedHandler(Object sender, EventArgs args)
+	{
+		//CreateUnitOnPosition(new UnityEngine.Vector3(0,0,0), Resources.Load("Prefab_AltyleryUnit"));
+	}
+	
+	//nie wiem jak to ma działać
+	private void UpdatePoints(Object sender, EventArgs args)
+	{
+		throw new NotImplementedException();			
+	}
+	
+	private void ContinueButtonClickedHandler(Object sender, EventArgs args)
+	{
+		 GameManager.Instance().GameControllerInstance.GoToInGameState();
+	}
 }
