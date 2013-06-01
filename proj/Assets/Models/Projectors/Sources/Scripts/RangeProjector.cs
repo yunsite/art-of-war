0211bfa9eee535d003c1687237307676e76d565a
zbyszekprasak <zbyszekprasak@gmail.com>
2013-06-01 15:21:46 +0000

    Implementacja zaznaczania jednostek.
    Unit: wprowadzono grupowanie statystyk, osobno statystyki ruchu, ataku, ataku specjalnego (w przyszłości) oraz stanu pojazdu.
    W każdej grupie znajdują się metdy związane z tymi statystykami.
    Prefab_SelectionMarker.prefab: dodano skrypt obsługujący tryby zaznaczania, i zawierający kolory przyporządkowane każdemu trybowi.
    Prefab_RangeProjector.prefab: dodano prefab wyznaczający zasięg ruchu, ataku itd., w skład którego wchodzi skrypt pozwalający na łarwe ustawianie promienia zasięgu i koloru okręgu granicznego.
    
    Zmodyfikowano prefaby Prefab_BWPUnit.prefab oraz Prefab_HeavyTankUnit.prefab, tak by miały marker zaznaczania i projektor zasięgu.
    
    Fixes Issue 51.

---
+++ proj/Assets/Models/Projectors/Sources/Scripts/RangeProjector.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Projector))]
+public class RangeProjector : MonoBehaviour {
+	
+	private Transform selfTransform;
+	private Projector projector;
+	
+	public float IntensityPercentage = 100;
+	public float Height = 20;
+	public float RangeRaius = 40;
+	public float RadiusOffsetPercentage = 5.5f;
+	
+	void Awake () {
+		selfTransform = transform;
+		projector = GetComponent<Projector>();
+	}
+	
+	void Start () {
+		if (Height <= 0) Height = 10;
+		if (RangeRaius <= 0) RangeRaius = Height;
+		SetRange(RangeRaius);
+	}
+	
+	public void SetRange(float radius) {
+		if (radius > 0) {
+			Vector3 position = selfTransform.position;
+			position.y = Terrain.activeTerrain.SampleHeight(position) + Height;
+			selfTransform.position = position;
+			projector.farClipPlane = (1f + IntensityPercentage / 100f) * Height;
+			RangeRaius = radius;
+			projector.fieldOfView = 2.0f * Mathf.Rad2Deg 
+				* Mathf.Atan(RangeRaius * (1f + RadiusOffsetPercentage / 100f) / Height);
+		}
+	}
+	
+	public void SetColor (Color color) {
+		projector.material.color = color;
+	}
+}
---
+++ proj/Assets/Models/SelectionMarker/Scripts/SelectionMarker.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+
+public class SelectionMarker : MonoBehaviour {
+	
+	public SelectionMode Mode;
+	public Color DefaultColor = new Color(35f / 255f, 106f / 255f, 251f / 255f, 1f);
+	public Color MovementColor = Color.green;
+	public Color AttackColor = Color.red;
+	public Color SpecialAttackColor = Color.yellow;
+	public Color NoActionColor = Color.gray;
+	public string MeshName = "markerMesh";
+	
+	private Material material;
+	
+	void Awake () {
+		Transform meshChild = transform.FindChild(MeshName);
+		if (meshChild == null) {
+			Debug.LogError("No required mesch child found.");
+			return;
+		}
+		
+		SkinnedMeshRenderer mesh = meshChild.GetComponent<SkinnedMeshRenderer>();
+		if (mesh == null) {
+			Debug.LogError("No required mesch renderer component found.");
+			return;
+		}
+		
+		material = mesh.material;
+		if (material == null) {
+			Debug.LogError("No required material found.");
+			return;
+		}
+	}
+	
+	void Start () {
+		SetMode(Mode);
+	}
+	
+	public Color GetModeColor (SelectionMode mode) {
+		switch (mode) {
+		case SelectionMode.Default:
+			return DefaultColor;
+		case SelectionMode.Attack:
+			return AttackColor;
+		case SelectionMode.Movement:
+			return MovementColor;
+		case SelectionMode.SpecialAttack:
+			return SpecialAttackColor;
+		case SelectionMode.NoAction:
+			return NoActionColor;
+		default:
+			throw new InvalidProgramException("Invalid program path reached.");
+		}
+	}
+	
+	public void SetMode (SelectionMode mode) {
+		material.color = GetModeColor(mode);
+	}
+}
+
+public enum SelectionMode { Default, Movement, Attack, SpecialAttack, NoAction };
