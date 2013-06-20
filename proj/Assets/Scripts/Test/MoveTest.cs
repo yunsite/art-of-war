1f73ff58d370961a1ee8f1b94021b0e2e5d45eb8
zbyszekprasak <zbyszekprasak@gmail.com>
2013-06-20 19:39:20 +0000

    Poprawa poruszania jednostkami. Dotyczy Issue 017.

---
+++ proj/Assets/Scripts/Test/MoveTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTest : MonoBehaviour {
+
+    public MotionController unit;
+    public PositionObserver ground;
+
+	// Use this for initialization
+	void Start () {
+        ground.Clicked += ground_Clicked;
+	}
+
+    void ground_Clicked(object sender, PositionEventArgs e)
+    {
+        unit.Move(e.Position);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
